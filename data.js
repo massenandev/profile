564d4e67e4372fcb628e1bc46673341d858886c7
Leticia Massenan <massenandev@gmail.com>
2020-02-29 22:19:17 -0300

    Using Nunjucks

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "b4GPyuqBsuA",
+        title: "Code | Development",
+        type: "Language:",
+        language: "NodeJS"
+    },
+    {
+        id: "8yjkWGRlUmY",
+        title: "Code | Development",
+        type: "Language:",
+        language: "Java"
+    },
+    {
+        id: "sc5RhTIBf4c",
+        title: "Code | Development",
+        type: "Language:",
+        language: "Ruby"
+    },
+    {
+        id: "ZDa-Z5JzLYM",
+        title: "Code | Development",
+        type: "Language:",
+        language: "Python"
+    },
+    {
+        id: "KDlQE1_fVmU",
+        title: "Code | Development",
+        type: "Language:",
+        language: ".Net"
+    },
+    {
+        id: "t2SPg6IuT3k",
+        title: "Code | Development",
+        type: "Language:",
+        language: "C#"
+    }
+]
